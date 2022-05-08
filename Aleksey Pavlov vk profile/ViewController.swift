072503e410eecfbe7eb3bdbe71c888179e08a4fe
fomka94 <fom-aina@mail.ru>
2022-05-08 17:22:12 +0900

    Initial Commit

---
+++ Aleksey Pavlov vk profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aleksey Pavlov vk profile
+//
+//  Created by Фомина Айина Валерьевна on 08.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Aleksey Pavlov vk profileTests/Aleksey_Pavlov_vk_profileTests.swift
@@ -0,0 +1,33 @@
+//
+//  Aleksey_Pavlov_vk_profileTests.swift
+//  Aleksey Pavlov vk profileTests
+//
+//  Created by Фомина Айина Валерьевна on 08.05.2022.
+//
+
+import XCTest
+@testable import Aleksey_Pavlov_vk_profile
+
+class Aleksey_Pavlov_vk_profileTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
